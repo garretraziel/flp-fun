8d044e9ef804cadc6931b388c4aaebec997a15ae
garretraziel <boloomka@gmail.com>
2014-04-20 17:43:10 +0200

    pridan jeden ukazkovy program

---
+++ example.c
@@ -0,0 +1,38 @@
+double PI;
+
+int factorial(int n);
+
+string uvitani(string a, int b) {
+	print("Dobry den "+a);
+	print("Faktorial zadaneho cisla je:");
+        print(factorial(b));
+	print("A hodnota PI je:");
+	print(PI);
+        return a;
+}
+
+int factorial(int n)
+{
+	int result;
+	if (n < 2) {
+		return 1;
+	} else {
+		return n * factorial(n-1);
+	}
+}
+
+int main()
+{
+	string a;
+        int b;
+	PI = 3.141592;
+	print("Zadejte vase jmeno:");
+	scan(a);
+        print("Zadejte cislo pro vypocet faktorialu:");
+	scan(b);
+	if (b < 0) {
+		print("Faktorial nelze spocitat.");
+	} else {
+		uvitani(a, b);
+	}
+}
